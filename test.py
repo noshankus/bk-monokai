de7038f2d6a75ad2912ae39e020677bf93fce8b8
ebriken <brian.kenny@ericsson.com>
2016-11-02 16:41:15 +0000

    update

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 
 # This is a comment
+# Missing syntax highlighting:
+# 1) equals sign is not included in operators, while only the exclamation mark of != is...
+# 2) "in" and "not" are defined as "cm-keyword"
+# 2) "if", "in" and "not" are defined as "cm-keyword", just like "def" - perhaps a "cm-keyword-2" might be appropriate?
+# 3) custom functions are identified as variables (see goto())
+# 4) classes during instantiation are identified as variables - poor readability
+# 5)
 
 moo = "Hello"
 
@@ -9,17 +16,33 @@ def goto(passed_derp):
 
 if moo == "Hello":
     goto(moo)
+if moo != "Hello":
+    goto(moo)
+
+ccc = None
 
 number = 43210
-operator = 1
+text_operators = list(1, 32, 43210)
 
-if operator !== number:
+if number not in text_operators:
     print("Nope")
 
 beep = dict(
-    aaa = dict(id='AAA', type='upper', true=True),
-    bbb = dict(id='bbb', type=3, true=False)
+    aaa = dict(
+        id   = 'AAA',
+        type = 'upper',
+        true = True
+    ),
+    bbb = {
+        "id":   'bbb',
+        "type": 3,
+        "true": False
+    }
 )
+
+print(beep.aaa.id)
+print(beep.bbb.id)
+
 
 class TestClass(object):
     def __init__(self, passed_boop):
